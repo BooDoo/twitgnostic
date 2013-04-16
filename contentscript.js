822918e4121a769eed2769759a6327603017a656
BooDoo <joelmccoy@gmail.com>
2013-04-16 17:18:46 -0400

    Fixed convos, replies; removed console output; minified

--- contentscript.js
+++ contentscript.js
@@ -1,14 +1,6 @@
-//Inject and run livequery, and then remove it.
+//Inject and run twitgnostic, and then remove script element.
 var s = document.createElement('script');
-	s.src = chrome.extension.getURL('livequery-111.min.js');
-	s.onload = function() {
-		this.parentNode.removeChild(this);
-	};
-(document.head||document.documentElement).appendChild(s);
-
-//Inject and run twitgnostic, and then remove it.
-var s = document.createElement('script');
-	s.src = chrome.extension.getURL('anon-timeline.js');
+	s.src = chrome.extension.getURL('anon-timeline.min.js');
 	s.onload = function() {
 		this.parentNode.removeChild(this);
 	};
